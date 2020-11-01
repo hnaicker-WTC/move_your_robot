b4abbb2ab291291fd01eb8317c9006ae5fb29df0
hnaicker_cpt <hnaicker@student.wethinkcode.co.za>
2020-11-01 10:31:22 +0200

    wtc-lms save

--- maze/obstacles.py
+++ maze/obstacles.py
@@ -1,19 +1,80 @@
-# -------------------------------------------------------------------------------------------------
-#
-# TODO: Please replace this placeholder code with your solution for Toy Robot 4, and work from there.
-#
-# -------------------------------------------------------------------------------------------------
+import random
 
-def create_random_obstacles():
+x = 0
+y = 0
+
+def create_random_obstacles(): 
     pass
 
 
 def is_position_blocked(x, y):
-    pass
+    '''
+    Checks to see if destined x and y coords cross over any of
+    the obstacle coords.
+    :param: x
+    :param: y
+    :return: True if there is an obstacle 
+    '''
+    obstacle_present = False
+
+    for obstacle in obstacles:
+        if x in range(obstacle[0], obstacle[0] + 4) and y in range(obstacle[1], obstacle[1] + 4):
+            obstacle_present = True
+
+    return obstacle_present
 
 
 def is_path_blocked(x1, y1, x2, y2):
-    pass
+    '''
+    Looks ahead to see if destined position will come into contact
+    with an obstacle.
+    :param: x1,y1: original x and y pos
+    :param: x2,y2: potentially destined x and y pos
+    :return: True if there is an obstacle along the path
+    '''
+
+    path_is_blocked = False
+
+    # print("In is path blocked obstacles: ", obstacles)
+    # print("In is path blocked ", x1,y1,x2,y2)
+
+    if x1 == x2:
+        #only asseses obstacles moving vertically
+        for obstacle in obstacles:
+            if obstacle[1] >= min(y1,y2) and obstacle[1] <= max(y1,y2):
+                for current_y_pos in range (min(y1,y2), max(y1,y2)):
+                    if is_position_blocked(x2, current_y_pos, obstacles) == True:
+                        path_is_blocked = True
+
+    if y1 == y2:
+        #only asseses obstacles moving horizontally
+        for obstacle in obstacles:
+            if obstacle[0] >= min(x1,x2) and obstacle[0] <= max(x1,x2):
+                for current_x_pos in range (min(x1,x2), max(x1,x2)):
+                    if is_position_blocked(current_x_pos, y1, obstacles) ==True:
+                        path_is_blocked = True
+
+    return path_is_blocked
+
 
 def get_obstacles():
-    pass
+    '''
+    Creates a random list of tuples containing one set of
+    coordinates of each obstacle
+    '''
+    #generates random list of length <10 of random coordinates stored in tuples
+    obstacles = [(random.randint(-100,100), random.randint(-200,200)) 
+    for coord in range(random.randint(0,10))]
+
+    return obstacles
+
+
+def print_obstacles(obstacles):
+    if(obstacles == None or len(obstacles) == 0):
+        return
+
+    print("There are some obstacles:")
+
+    for obstacle in obstacles:
+        print('- At position {},{} (to {},{})'.format(obstacle[0], obstacle[1], 
+        obstacle[0]+4, obstacle[1]+4))
--- robot.py
+++ robot.py
@@ -1,15 +1,319 @@
-# -------------------------------------------------------------------------------------------------
-#
-# TODO: Please replace this placeholder code with your solution for Toy Robot 4, and work from there.
-#
-# -------------------------------------------------------------------------------------------------
+import sys
+import world.text.world as blueworld
+import world.turtle.world as turtleworld
 
-def robot_start():
+
+# list of valid command names
+valid_commands = ['off', 'help', 'replay', 'forward', 'back', 'right', 'left', 
+'sprint']
+move_commands = valid_commands[3:]
+
+# variables tracking position and direction
+directions = ['forward', 'right', 'back', 'left']
+
+environment = None
+
+# area limit vars
+min_y, max_y = -200, 200
+min_x, max_x = -100, 100
+
+#commands history
+history = []
+
+
+def get_robot_name():
+    name = input("What do you want to name your robot? ")
+    while len(name) == 0:
+        name = input("What do you want to name your robot? ")
+    return name
+
+
+def get_command(robot_name):
     """
-    Replace me!
+    Asks the user for a command, and validate it as well
+    Only return a valid command
     """
-    pass
+
+    prompt = ''+robot_name+': What must I do next? '
+    command = input(prompt)
+    while len(command) == 0 or not valid_command(command):
+        output(robot_name, "Sorry, I did not understand '"+command+"'.")
+        command = input(prompt)
+
+    return command.lower()
+
+
+def split_command_input(command):
+    """
+    Splits the string at the first space character, to get the actual command, 
+    as well as the argument(s) for the command
+    :return: (command, argument)
+    """
+    args = command.split(' ', 1)
+    if len(args) > 1:
+        return args[0], args[1]
+    return args[0], ''
+
+
+def is_int(value):
+    """
+    Tests if the string value is an int or not
+    :param value: a string value to test
+    :return: True if it is an int
+    """
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def valid_command(command):
+    """
+    Returns a boolean indicating if the robot can understand the command or not
+    Also checks if there is an argument to the command, and if it a valid int
+    """
+
+    (command_name, arg1) = split_command_input(command)
+
+    if command_name.lower() == 'replay':
+        if len(arg1.strip()) == 0:
+            return True
+        elif (arg1.lower().find('silent') > -1 or arg1.lower().find('reversed') > -1) and len(arg1.lower().replace('silent', '').replace('reversed','').strip()) == 0:
+            return True
+        else:
+            range_args = arg1.replace('silent', '').replace('reversed','')
+            if is_int(range_args):
+                return True
+            else:
+                range_args = range_args.split('-')
+                return is_int(range_args[0]) and is_int(range_args[1]) and len(range_args) == 2
+    else:
+        return command_name.lower() in valid_commands and (len(arg1) == 0 or is_int(arg1))
+
+
+def output(name, message):
+    print(''+name+": "+message)
+
+
+def do_help():
+    """
+    Provides help information to the user
+    :return: (True, help text) to indicate robot can continue after this command was handled
+    """
+    return True, """I can understand these commands:
+OFF  - Shut down robot
+HELP - provide information about commands
+FORWARD - move forward by specified number of steps, e.g. 'FORWARD 10'
+BACK - move backward by specified number of steps, e.g. 'BACK 10'
+RIGHT - turn right by 90 degrees
+LEFT - turn left by 90 degrees
+SPRINT - sprint forward according to a formula
+REPLAY - replays all movement commands from history [FORWARD, BACK, RIGHT, LEFT, 
+SPRINT]
+"""
+
+
+def do_forward(robot_name, steps, obstacles):
+    """
+    Moves the robot forward the number of steps
+    :param robot_name:
+    :param steps:
+    :param obstacles:
+    :return: (True, forward output text)
+    """
+    global environment
+
+    can_update_position, reason = environment.update_position(steps, obstacles)
+
+    if can_update_position:
+        return True, ' > '+robot_name+' moved forward by '+str(steps)+' steps.'
+    else:
+        if reason == "obstacles":
+            return True, ''+robot_name+': Sorry, there is an obstacle in the way'
+        else:
+            return True, ''+robot_name+': Sorry, I cannot go outside my safe zone.'
+
+
+def do_back(robot_name, steps, obstacles):
+    """
+    Moves the robot forward the number of steps
+    :param robot_name:
+    :param steps:
+    :return: (True, forward output text)
+    :param obstacles:
+    """
+    global environment
+
+    if environment.update_position(-steps, obstacles):
+        return True, ' > '+robot_name+' moved back by '+str(steps)+' steps.'
+    else:
+        return True, ''+robot_name+': Sorry, I cannot go outside my safe zone.'
+
+
+def do_sprint(robot_name, steps, obstacles):
+    """
+    Sprints the robot, i.e. let it go forward steps + (steps-1) + (steps-2) + 
+    .. + 1 number of steps, in iterations
+    :param robot_name:
+    :param steps:
+    :param obstacles:
+    :return: (True, forward output)
+    """
+
+    if steps == 1:
+        return do_forward(robot_name, 1, obstacles)
+    else:
+        (do_next, command_output) = do_forward(robot_name, steps, obstacles)
+        print(command_output)
+        return do_sprint(robot_name, steps - 1, obstacles)
+
+
+def get_commands_history(reverse, relativeStart, relativeEnd):
+    """
+    Retrieve the commands from history list, already breaking them up into 
+    (command_name, arguments) tuples
+    :param reverse: if True, then reverse the list
+    :param relativeStart: the start index relative to the end position of 
+    command, e.g. -5 means from index len(commands)-5; None means from beginning
+    :param relativeEnd: the start index relative to the end position of command,
+    e.g. -1 means from index len(commands)-1; None means to the end
+    :return: return list of (command_name, arguments) tuples
+    """
+
+    commands_to_replay = [(name, args) for (name, args) in list(map(lambda command: split_command_input(command), history)) if name in move_commands]
+    if reverse:
+        commands_to_replay.reverse()
+
+    range_start = len(commands_to_replay) + relativeStart if (relativeStart is not None and (len(commands_to_replay) + relativeStart) >= 0) else 0
+    range_end = len(commands_to_replay) + relativeEnd if  (relativeEnd is not None and (len(commands_to_replay) + relativeEnd) >= 0 and relativeEnd > relativeStart) else len(commands_to_replay)
+    return commands_to_replay[range_start:range_end]
+
+
+def do_replay(robot_name, arguments, obstacles):
+    """
+    Replays historic commands
+    :param robot_name:
+    :param arguments a string containing arguments for the replay command
+    :param obstacles:
+    :return: True, output string
+    """
+    global environment
+
+    silent = arguments.lower().find('silent') > -1
+    reverse = arguments.lower().find('reversed') > -1
+    range_args = arguments.lower().replace('silent', '').replace('reversed', '')
+
+    range_start = None
+    range_end = None
+
+    if len(range_args.strip()) > 0:
+        if is_int(range_args):
+            range_start = -int(range_args)
+        else:
+            range_args = range_args.split('-')
+            range_start = -int(range_args[0])
+            range_end = -int(range_args[1])
+
+    commands_to_replay = get_commands_history(reverse, range_start, range_end)
+
+    for (command_name, command_arg) in commands_to_replay:
+        (do_next, command_output) = call_command(command_name, command_arg, robot_name, obstacles)
+        if not silent:
+            print(command_output)
+            environment.show_position(robot_name)
+
+    return True, ' > '+robot_name+' replayed ' + str(len(commands_to_replay)) + ' commands' + (' in reverse' if reverse else '') + (' silently.' if silent else '.')
+
+
+def call_command(command_name, command_arg, robot_name, obstacles):
+    global environment
+
+    if command_name == 'help':
+        return do_help()
+    elif command_name == 'forward':
+        return do_forward(robot_name, int(command_arg), obstacles)
+    elif command_name == 'back':
+        return do_back(robot_name, int(command_arg), obstacles)
+    elif command_name == 'right':
+        return environment.do_right_turn(robot_name)
+    elif command_name == 'left':
+        return environment.do_left_turn(robot_name)
+    elif command_name == 'sprint':
+        return do_sprint(robot_name, int(command_arg), obstacles)
+    elif command_name == 'replay':
+        return do_replay(robot_name, command_arg, obstacles)
+    return False, None
+
+
+def handle_command(robot_name, command, obstacles):
+    """
+    Handles a command by asking different functions to handle each command.
+    :param robot_name: the name given to robot
+    :param command: the command entered by user
+    :param obstacles:
+    :return: `True` if the robot must continue after the command, or else 
+    `False` if robot must shutdown
+    """
+    global environment
+    
+    (command_name, arg) = split_command_input(command)
+
+    if command_name == 'off':
+        return False
+    else:
+        (do_next, command_output) = call_command(command_name, arg, robot_name, obstacles)
+
+    print(command_output)
+    environment.show_position(robot_name)
+    add_to_history(command)
+
+    return do_next
+
+
+def add_to_history(command):
+    """
+    Adds the command to the history list of commands
+    :param command:
+    :return:
+    """
+    history.append(command)
+
+
+def robot_start(worldArg='text'):
+    """This is the entry point for starting my robot"""
+
+    global history, environment
+    # global position_x, position_y, current_direction_index, history, environment
+
+    robot_name = get_robot_name()
+    output(robot_name, "Hello kiddo!")
+
+    environment = blueworld
+    if worldArg == 'turtle':
+        environment = turtleworld
+
+    obstacles = environment.initialise()
+
+
+    history = []
+    
+    
+    command = get_command(robot_name)
+    while handle_command(robot_name, command, obstacles):
+        command = get_command(robot_name)
+
+    output(robot_name, "Shutting down..")
+    environment.teardown()
+    environment = None
 
 
 if __name__ == "__main__":
-    robot_start()
+
+    if len(sys.argv) == 1:
+        robot_start()
+    elif len(sys.argv) > 1:
+        if sys.argv[1] == 'turtle':
+            robot_start('turtle')
+        else:
+            robot_start()
